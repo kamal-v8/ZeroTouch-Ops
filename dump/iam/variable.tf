10eb7ce19dae3bdef5393e1cdd41c615f17d0c36
kamal <kamal.v3011f@gmail.com>
2026-04-10 21:07:15 +0530

    implementing kind

---
+++ dump/iam/variable.tf
@@ -0,0 +1,10 @@
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "environment" {
+  description = "environment name"
+  type        = string
+
+}
